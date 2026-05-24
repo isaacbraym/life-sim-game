3b07395b22b3b928b9703181a7a34dab74032b4f
isaacbraym <isaacbraym1@gmail.com>
2026-05-24 02:32:36 -0300

    feat: gerarPathMembroOrganico and gerarPathTronco in BezierSegment

--- packages/core/src/silhouette/index.ts
+++ packages/core/src/silhouette/index.ts
@@ -1,14 +1,22 @@
 export {
   gerarPathBraco,
+  gerarPathCabeca,
+  gerarPathMembroOrganico,
   gerarPathPerna,
+  gerarPathTronco,
+  lerpPonto,
+  normalPerpendicular,
   PERFIL_BRACO_PADRAO,
   PERFIL_PERNA_PADRAO,
   PERFIL_TRONCO_PADRAO,
 } from './BezierSegment';
 
 export type {
+  PerfilMembro,
   PerfilBraco,
   PerfilPerna,
   PerfilTronco,
+  Ponto,
   PathBraco,
+  SegmentoPath,
 } from './BezierSegment';
